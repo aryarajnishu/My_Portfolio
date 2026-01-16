5ff3efc6aadd0b586fc2feaaad9553a47d1cb02c
aryarajnishu <22bds007@iiitdwd.ac.in>
2026-01-16 10:35:05 +0530

    resume Update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1aB_y48CL6Yc7U2VjIEWLpX4qo6AwkxpZ/view?usp=sharing"
+                href="https://drive.google.com/file/d/16pjcO1-TAAEPZHqordtEI_OQOx2_EucX/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 See My CV
